4f86a82d40e0837a88cc63e88ca56c2d67802b81
haydcmn <chanchan.25@ichat.sp.edu.sg>
2026-05-20 21:31:24 +0800

    Modified calculate_bmi() to return weight classification codes

--- bmi.py
+++ bmi.py
@@ -1,5 +1,52 @@
-/* def calculate_bmi(height, weight):
-    print("Height = " + height)
-    print("Weight = " + weight)
+#(a) 
+#    def calculate_bmi(height, weight):
+#    print("Height = " + height)
+#    print("Weight = " + weight)
 
-calculate_bmi(weight="57", height="1.73") */
+#     calculate_bmi(weight="57", height="1.73") */
+
+#(b) 
+#    def calculate_bmi(height, weight):
+#    print("Height = " + height)
+#    print("Weight = " + weight)
+#
+#    calculate_bmi(weight=57, height=1.73)
+
+#(c)
+#    def calculate_bmi(height, weight):
+#    print("Height = " + str(height))
+#    print("Weight = " + str(weight))
+
+#    calculate_bmi(weight=57, height=1.73)
+
+#(d)
+#def calculate_bmi(height, weight):
+#    print("Height = " + str(height))
+#   print("Weight = " + str(weight))
+
+#    bmi = weight / (height * height)
+
+#    print("BMI = " + str(bmi))
+
+#   if bmi < 18.5:
+#        print("Under Weight")
+
+#    elif bmi >= 18.5 and bmi <= 25:
+#       print("Normal Weight")
+
+#    else:
+#        print("Over Weight")
+
+#calculate_bmi(weight=57, height=1.73)
+
+#(e)
+
+def calculate_bmi(height, weight):
+    bmi = weight / (height * height)
+    
+    if bmi < 18.5:
+        return -1  # Under weight
+    elif 18.5 <= bmi <= 25.0:
+        return 0   # Normal weight
+    else:
+        return 1   # Over weight
